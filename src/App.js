bee75f1dce075372b5dd03b9fbc7a45481a79293
Fat Ape Rocket Crew <simeonkirilov88@gmail.com>
2021-10-23 02:58:59 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -215,8 +215,8 @@ function App() {
             style={{
               backgroundColor: "var(--accent)",
               padding: 24,
-              borderRadius: 24,
-              border: "4px dashed var(--secondary)",
+              borderRadius: 10,
+              border: "1px dashed var(--secondary)",
               boxShadow: "0px 5px 11px 2px rgba(0,0,0,0.7)",
             }}
           >
