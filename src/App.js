5bdbe7434916bfcb3f5debec3233a71fde13e388
Fat Ape Rocket Crew <simeonkirilov88@gmail.com>
2021-10-23 02:55:55 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -439,44 +439,102 @@ function App() {
 
           <s.SpacerLarge />
               
-                   <s.TextDescription
+                  
+          <s.Container flex={2} jc={"right"} ai={"right"}>
+                    <StyledImg
+                      alt={"example"}
+                      src={"/config/images/example.gif"}
+                      style={{ transform: "scaleX(-1)" }}
+                     />
+
+
+                     <s.TextDescription
                       style={{
                         textAlign: "center",
                         color: "var(--primary-text)",
                       }}
-                    >
+                      >
+                        
                       Please make sure you are connected to the right network (
                       {CONFIG.NETWORK.NAME} Mainnet) and the correct address. Please note:
                       Once you make the purchase, you cannot undo this action.
+                      
                     </s.TextDescription>
-          <s.Container flex={2} jc={"right"} ai={"right"}>
-            <StyledImg
-              alt={"example"}
-              src={"/config/images/example.gif"}
-              style={{ transform: "scaleX(-1)" }}
-            />
           </s.Container>
 
 
             <s.SpacerLarge />
-          <s.Container flex={2} jc={"left"} ai={"left"}>
-            <StyledImg
-              alt={"example"}
-              src={"/config/images/example.gif"}
-              style={{ transform: "scaleX(-1)" }}
-            />
-               <s.TextDescription
+                        
+                        
+           <s.Container flex={2} jc={"right"} ai={"right"}>
+                    <StyledImg
+                      alt={"example"}
+                      src={"/config/images/example.gif"}
+                      style={{ transform: "scaleX(-1)" }}
+                     />
+
+
+                     <s.TextDescription
                       style={{
                         textAlign: "center",
                         color: "var(--primary-text)",
                       }}
-                    >
+                      >
+                        
                       Please make sure you are connected to the right network (
                       {CONFIG.NETWORK.NAME} Mainnet) and the correct address. Please note:
                       Once you make the purchase, you cannot undo this action.
+                      
                     </s.TextDescription>
           </s.Container>
 
+
+
+          <s.Container flex={4} jc={"right"} ai={"right"}>
+                     <s.TextDescription
+                      style={{
+                        textAlign: "center",
+                        color: "var(--primary-text)",
+                      }}
+                      >
+                        
+                      Please make sure you are connected to the right network (
+                      {CONFIG.NETWORK.NAME} Mainnet) and the correct address. Please note:
+                      Once you make the purchase, you cannot undo this action.
+                      
+                    </s.TextDescription>
+
+                    <s.TextDescription
+                      style={{
+                        textAlign: "center",
+                        color: "var(--primary-text)",
+                      }}
+                      >
+                        
+                      Please make sure you are connected to the right network (
+                      {CONFIG.NETWORK.NAME} Mainnet) and the correct address. Please note:
+                      Once you make the purchase, you cannot undo this action.
+                      
+                    </s.TextDescription>
+
+
+                     <s.TextDescription
+                      style={{
+                        textAlign: "center",
+                        color: "var(--primary-text)",
+                      }}
+                      >
+                        
+                      Please make sure you are connected to the right network (
+                      {CONFIG.NETWORK.NAME} Mainnet) and the correct address. Please note:
+                      Once you make the purchase, you cannot undo this action.
+                      
+                    </s.TextDescription>
+          </s.Container>
+
+
+
+          
 
       </s.Container>
     </s.Screen>
